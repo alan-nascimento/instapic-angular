828743da4042598068f31ad2fa3eb0b8819ec35b
Alan Nascimento <alan.silva.n@hotmail.com>
2019-07-02 00:51:39 -0300

    adding photos module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { PhotoService } from './photos/photo/photo.service';
+import { Photo } from './photos/photo/photo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'instapic';
+export class AppComponent implements OnInit {
+
+  photos: Photo[] = [];
+  
+  constructor(private photoService: PhotoService) {}
+  
+  ngOnInit() {
+    this.photoService
+      .listFromUser('alan')
+      .subscribe(photos => this.photos = photos);
+  }
 }
